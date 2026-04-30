3e31647118466c407f589cc83ccc19b789570388
Sourav Nandi <sn9378115@gmail.com>
2026-04-30 21:06:19 +0530

    fix:"fix the layout ofthe header"

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header({ total }) {
   return (
     <div className="bg-white shadow-md rounded-2xl p-6 border border-slate-200">
-      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
-        <div>
+      <div className="flex flex-col justify-center items-center sm:flex-row sm:items-center sm:justify-between gap-4">
+        <div className="flex flex-col justify-center items-center">
           <h1 className="text-3xl font-bold text-slate-800">Job Tracker</h1>
           <p className="text-slate-500 mt-1">
             Track all the jobs you apply for
